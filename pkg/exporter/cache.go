4086fdadc63c0e2099050bb694f69f4a2b1fdad2
ricoberger <mail@ricoberger.de>
2024-11-10 11:04:46 +0100

    Fix Race Condition when Cache is Used
    When to requests were accessing the cache during the same time, there
    was a race condition because of the concurrent access to the map which
    holds the cached entries.
    
    This is now fixed by adding a lock for the cache, so that multiple
    requests can not access the cache in parallel.

--- pkg/exporter/cache.go
+++ pkg/exporter/cache.go
@@ -3,10 +3,12 @@ package exporter
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 )
 
 var cache map[string]cacheEntry
+var cacheLock = sync.RWMutex{}
 
 type cacheEntry struct {
 	cacheTime       time.Time
@@ -16,6 +18,9 @@ type cacheEntry struct {
 }
 
 func getCacheResult(scriptName string, paramValues []string, cacheDuration time.Duration, expCacheOnTimeout bool) (*string, *int, *int) {
+	cacheLock.RLock()
+	defer cacheLock.RUnlock()
+
 	if entry, ok := cache[fmt.Sprintf("%s--%s", scriptName, strings.Join(paramValues, "-"))]; ok {
 		if entry.cacheTime.Add(cacheDuration).After(time.Now()) || expCacheOnTimeout {
 			return &entry.formattedOutput, &entry.successStatus, &entry.exitCode
@@ -26,6 +31,9 @@ func getCacheResult(scriptName string, paramValues []string, cacheDuration time.
 }
 
 func setCacheResult(scriptName string, paramValues []string, formattedOutput string, successStatus, exitCode int) {
+	cacheLock.Lock()
+	defer cacheLock.Unlock()
+
 	if cache == nil {
 		cache = make(map[string]cacheEntry)
 	}
